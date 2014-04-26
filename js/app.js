54153ae536c5fe50932d07db3ffa931a6042fcbe
Hoke <m@h0ke.com>
2014-04-26 15:21:10 -0700

    added filtering and made the frontend look like a super sweet site

--- js/app.js
+++ js/app.js
@@ -1,35 +1,61 @@
 var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([37.8102589045, -122.265385309], 12)
   , featureLayer;
-var popup = L.popup({offset: L.point(0, -30)});
 
-$(document).ready(function() {
-  $.getJSON('http://localhost:8000/', function(data) {
-    var geojson = {
-      type: 'FeatureCollection',
-      features: data
-    };
+function buildPopup(props) {
+  return '<h1>' + props.Address + '</h1>' +
+    'Category: ' + props.SpecUse + '<br/>' +
+    'Acres: ' + props.Acre + '<br/>' +
+    'Square Ft: ' + props["SqFt\r"] + '<br/>' +
+    'FID Parcel: ' + props.FID_PARCEL;
+}
 
-    featureLayer = L.mapbox.featureLayer(geojson)
-      .on('click', function(e) {
-        var props = e.layer.feature.properties;
-        var content = '<h1>' + props.Address + '</h1>' +
-          'Category: ' + props.SpecUse + '<br/>' +
-          'Acres: ' + props.Acre + '<br/>' +
-          'Square Ft: ' + props["SqFt\r"] + '<br/>' +
-          'FID Parcel: ' + props.FID_PARCEL;
+// extend MarkerClusterGroup for filtering in clusters
+L.MarkerClusterGroup.include({
+  fromGeoJSON: function (geojson) {
+    this._geojson = geojson;
+    this.filter();
+  },
 
-        popup
-          .setContent(content)
-          .setLatLng(e.latlng)
-          .openOn(map)
-        ;
+  filter: function (f) {
+    f = f || function (m) { return true; }
+    var markers = Array();
+    for (var i = 0; i < this._geojson.features.length; i++) {
+      var a = this._geojson.features[i];
+      if (!f(a)) { continue; }
+      var title = a.properties['title'];
+      var description = a.properties['description']
+      var marker = L.marker(new L.LatLng(a.geometry.coordinates[1], a.geometry.coordinates[0]), {
+        icon: L.mapbox.marker.icon({'marker-symbol': a.properties['marker-symbol'], 'marker-color': a.properties['marker-color']}),
+        title: title
+      });
+     
+      marker.bindPopup(buildPopup(a.properties));
+      markers.push(marker);
+    }
+    this.clearLayers();
+    this.addLayers(markers);
+  }
+});
 
-      })
-    ;
+var cluster = new L.MarkerClusterGroup();
 
-    var markers = new L.MarkerClusterGroup();
-    markers.addLayer(featureLayer);
+$.getJSON('http://localhost:8000/', function(data) {
+  var geojson = {
+    type: 'FeatureCollection',
+    features: data
+  };
 
-    map.addLayer(markers);
+  map.addLayer(cluster);
+  cluster.fromGeoJSON(geojson); 
+
+});
+
+$('#controls').click("input", function (e) {
+  var child_categories = $(e.currentTarget).children(":checked").map(function (idx, c){
+    return $(c).val();
+  }).toArray();
+
+  cluster.filter(function(f) {
+    return child_categories.indexOf(f.properties['SpecUse']) >= 0;
   });
 });
