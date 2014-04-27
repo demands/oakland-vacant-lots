157027e85dc908213e92dea9a614afe84a854b11
Max Edmands <me@maxedmands.com>
2014-04-26 18:36:23 -0700

    Refactor for speed and readability

--- app.js
+++ app.js
@@ -1,65 +1,19 @@
-var $ = require('jquery');
 require('mapbox.js');
-require('leaflet.markercluster');
+require('./parcelClusters');
 
+var $ = require('jquery');
 
-var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([37.8102589045, -122.265385309], 12)
-  , featureLayer;
-
-function buildPopup(props) {
-  return '<h1>' + props.Address + '</h1>' +
-    'Category: ' + props.SpecUse + '<br/>' +
-    'Acres: ' + props.Acre + '<br/>' +
-    'Square Ft: ' + props["SqFt\r"] + '<br/>' +
-    'FID Parcel: ' + props.FID_PARCEL;
-}
-
-// extend MarkerClusterGroup for filtering in clusters
-L.MarkerClusterGroup.include({
-  fromGeoJSON: function (geojson) {
-    this._geojson = geojson;
-    this.filter();
-  },
-
-  filter: function (f) {
-    f = f || function (m) { return true; }
-    var markers = Array();
-    for (var i = 0; i < this._geojson.features.length; i++) {
-      var a = this._geojson.features[i];
-      if (!f(a)) { continue; }
-      var title = a.properties['title'];
-      var description = a.properties['description']
-      var marker = L.marker(new L.LatLng(a.geometry.coordinates[1], a.geometry.coordinates[0]), {
-        icon: L.mapbox.marker.icon({'marker-symbol': a.properties['marker-symbol'], 'marker-color': a.properties['marker-color']}),
-        title: title
-      });
-
-      marker.bindPopup(buildPopup(a.properties));
-      markers.push(marker);
-    }
-    this.clearLayers();
-    this.addLayers(markers);
-  }
-});
-
+var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([37.8102589045, -122.265385309], 12);
 var cluster = new L.MarkerClusterGroup();
 map.addLayer(cluster);
 
 $.getJSON('http://localhost:8000/', function(data) {
-  var geojson = {
-    type: 'FeatureCollection',
-    features: data
-  };
-
-  cluster.fromGeoJSON(geojson);
+  cluster.addBulk(data);
 });
 
 $('#controls').click("input", function (e) {
   var child_categories = $(e.currentTarget).children(":checked").map(function (idx, c){
     return $(c).val();
   }).toArray();
-
-  cluster.filter(function(f) {
-    return child_categories.indexOf(f.properties['SpecUse']) >= 0;
-  });
+  cluster.filterCategory(child_categories);
 });
---
+++ parcelClusters.js
@@ -0,0 +1,45 @@
+require('leaflet.markercluster');
+
+function popupHtml(properties) {
+  return '<h1>' + properties.Address + '</h1>' +
+    'Category: ' + properties.SpecUse + '<br/>' +
+    'Acres: ' + properties.Acre + '<br/>' +
+    'Square Ft: ' + properties["SqFt\r"] + '<br/>' +
+    'FID Parcel: ' + properties.FID_PARCEL;
+}
+
+function parcelMarker(geoJson) {
+  var latLng = new L.LatLng(
+    geoJson.geometry.coordinates[1],
+    geoJson.geometry.coordinates[0]
+  );
+  var marker = L.marker(latLng, {
+    icon: L.mapbox.marker.icon(),
+    title: geoJson.properties.Address
+  });
+  marker.bindPopup(popupHtml(geoJson.properties));
+  marker.category = geoJson.properties.SpecUse;
+  return marker;
+}
+
+L.MarkerClusterGroup.include({
+  addBulk: function(parcels) {
+    this._markers = parcels.map(function(parcel) {
+      return parcelMarker(parcel);
+    });
+    this.addLayers(this._markers);
+  },
+  filterCategory: function(categories) {
+    this._markers.forEach(function(marker) {
+      var showing = this.hasLayer(marker);
+      var shouldBeShowing = categories.indexOf(marker.category) >= 0;
+
+      if(showing && !shouldBeShowing) {
+        this.removeLayer(marker);
+      }
+      if(!showing && shouldBeShowing) {
+        this.addLayer(marker);
+      }
+    }.bind(this));
+  }
+});
