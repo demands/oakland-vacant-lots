3343cb07fe9b3a4d6ad3adc8fd3216367b76cc83
Max Edmands <me@maxedmands.com>
2014-06-07 09:45:17 -0700

    Very very basic browser test infrastructure

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var url = require('url');
+require('mapbox.js');
+
 var parsedUrl = url.parse(window.location.href, true);
 global.MAPBOX_MAP_ID = 'map-communities.ib23mbhk';
 global.SERVER_BASE_URL = 'http://localhost:8000';
 
 // TODO: get fancy w/ requiring the pathname
 if (parsedUrl.pathname == '/') {
-  require('./js/index')();
+  var map = L.mapbox.map('map', MAPBOX_MAP_ID).setView([37.8102589045, -122.265385309], 12);
+  require('./js/index')(map);
 } else if (parsedUrl.pathname == '/point.html') {
   require('./js/point')(parsedUrl);
 }
--- js/index.js
+++ js/index.js
@@ -2,19 +2,23 @@ require('mapbox.js');
 require('./parcelClusters');
 var $ = require('jquery');
 
-module.exports = function(){
-  var map = L.mapbox.map('map', MAPBOX_MAP_ID).setView([37.8102589045, -122.265385309], 12);
+module.exports = function(map){
+
   var cluster = new L.MarkerClusterGroup();
   map.addLayer(cluster);
 
   $.getJSON(SERVER_BASE_URL + '/points', function(data) {
+    console.log("adding markers", data);
     cluster.addBulk(data.features);
   });
 
   $('#controls').click("input", function (e) {
+    console.log("handling click");
     var child_categories = $(e.currentTarget).children(":checked").map(function (idx, c){
       return $(c).val();
     }).toArray();
     cluster.filterCategory(child_categories);
   });
+
+  return cluster;
 }
