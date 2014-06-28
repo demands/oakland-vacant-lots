873ce6a52f7f6119bd95fdbd6f276f7ec3820af1
Max Edmands <me@maxedmands.com>
2014-06-28 13:07:45 -0700

    Add a script prepopulate database with oakland city data from the internet

---
+++ bin/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if ! $(which -s mongo); then
+  echo "You need mongodb on your system! If you're on a mac, try \`brew install mongodb\`." 1>&2
+  exit 1
+fi
+
+curl https://raw.githubusercontent.com/map-communities/oakland-vacant-lots/master/oakland-vacant-lots.geojson | ./node_modules/.bin/map-communities-import-mongodb oakland-map
